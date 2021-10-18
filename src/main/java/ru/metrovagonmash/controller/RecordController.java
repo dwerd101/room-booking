e4ea32f0278ff8c832150110f0728143545b20d9
Andrey <andrey.mail.12@gmail.com>
2021-10-18 22:43:01 +0300

    merged

--- src/main/java/ru/metrovagonmash/controller/RecordController.java
+++ src/main/java/ru/metrovagonmash/controller/RecordController.java
@@ -1,6 +1,8 @@
 package ru.metrovagonmash.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
